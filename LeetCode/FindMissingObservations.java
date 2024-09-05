ef86f385b6cfd451376e92b3a1af9943c98bf6c6
ishantSikdar <ishant9715@gmail.com>
2024-09-05 15:46:33 +0530

    find missing observations

---
+++ LeetCode/FindMissingObservations.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int m = rolls.length;
+        int totalSum = mean * (m + n);
+        int missingSum = 0;
+
+        for (int roll: rolls) {
+            missingSum += roll;
+        }
+
+        missingSum = totalSum - missingSum;
+
+        if (missingSum < n || missingSum > n * 6) {
+            return new int[0];
+        }
+
+        int base = missingSum / n;
+        int remainder = missingSum % n;
+        int[] missing = new int[n];
+
+        for (int i = 0; i<n; i++) {
+            missing[i] = base + (i < remainder ? 1 : 0);
+        }
+
+        return missing;
+    }
+}
