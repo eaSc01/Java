6de71708fdca9bdae5e9c66690a35044e76155a5
ishantSikdar <ishant9715@gmail.com>
2024-09-15 23:28:24 +0530

    Longest Substring Vowels With Even Count

---
+++ LeetCode/LongestSubstringVowelsEvenCounts.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        int xor = 0, len = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+        for (int i = 0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u') {
+                xor = xor ^ c;
+            }
+
+            if (map.containsKey(xor)) {
+                len = Math.max(len, i - map.get(xor));
+            } else {
+                map.put(xor, i);
+            }
+        }
+
+        return len;
+    }
+}
