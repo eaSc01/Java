638974831d27ea20f82420cd48b022fe40289769
ishantSikdar <ishant9715@gmail.com>
2024-09-16 22:12:13 +0530

    ATOI

---
+++ LeetCode/MinMinutesDifference.java
@@ -0,0 +1,30 @@
+package LeetCode;
+import java.util.*;
+
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        int[] timeMinutes = new int[timePoints.size()];
+
+        for (int i = 0; i<timeMinutes.length; i++) {
+            String[] time = timePoints.get(i).split(":");
+            int hours = Integer.parseInt(time[0]);
+            int mins = Integer.parseInt(time[1]);
+            int totalMins = mins + (hours * 60);
+            timeMinutes[i] = totalMins;
+        }
+
+        Arrays.sort(timeMinutes);
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 1; i<timeMinutes.length; i++) {
+            int diff = timeMinutes[i] - timeMinutes[i - 1];
+            ans = Math.min(ans, diff);
+        }
+
+        return Math.min(
+            ans,
+            24 * 60 - timeMinutes[timeMinutes.length - 1] + timeMinutes[0]
+        );
+    }
+}
---
+++ LeetCode/StringToInt.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.length() == 0) {
+            return 0;
+        }
+        
+        int value = 0;
+        int i = 0;
+        boolean isNegative = false;
+
+        if (s.charAt(0) == '-') {
+            isNegative = true;
+            i++;
+        } else if (s.charAt(0) == '+') {
+            i++;
+        }
+
+        for (; i<s.length(); i++) {
+            char c = s.charAt(i);
+            int num = c - '0';
+
+            if (num > 9 || num < 0) {
+                break;
+            }
+
+            if (value > (Integer.MAX_VALUE - num)/10) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            value = (value * 10) + num;
+        }
+
+        return isNegative ? -value : value;
+    }
+}
