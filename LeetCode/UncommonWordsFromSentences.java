bbded2e3db923056517394fec5a86eaa61367989
ishantSikdar <ishant9715@gmail.com>
2024-09-17 22:11:24 +0530

    Uncommon words from sentences

---
+++ LeetCode/UncommonWordsFromSentences.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        
+        for (String word : s1.split(" ")) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        for (String word : s2.split(" ")) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        List<String> uncommon = new ArrayList<>();
+        for (String word: wordCount.keySet()) {
+            if (wordCount.get(word) == 1) {
+                uncommon.add(word);
+            }
+        }
+
+        return uncommon.toArray(new String[0]);
+    }
+}
