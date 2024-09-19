414e83bb9fe4f3d6b7597d0da36eeddcc8b43915
ishantSikdar <ishant9715@gmail.com>
2024-09-19 19:26:12 +0530

    Strings

---
+++ Strings/CountAndSay.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 1; i< n; i++) {
+            s = rle(s);
+        }
+        return s;
+    }
+
+    public String rle(String s) {
+        StringBuilder sb = new StringBuilder("");
+        int count = 1;
+
+        for (int i = 1; i<s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+
+            } else {
+                sb.append(String.valueOf(count));
+                sb.append(s.charAt(i - 1));
+                count = 1;
+            }
+        }
+
+        sb.append(String.valueOf(count));
+        sb.append(s.charAt(s.length() - 1));
+
+        return sb.toString();
+    }
+}
---
+++ Strings/DifferentWaysToAddParenthesis.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i<expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            if (c == '*' || c == '+' || c == '-') {
+                String left = expression.substring(0, i);
+                String right = expression.substring(i + 1);
+
+                List<Integer> leftAns = diffWaysToCompute(left);
+                List<Integer> rightAns = diffWaysToCompute(right);
+
+                for (int a: leftAns) {
+                    for (int b: rightAns) {
+                        if (c == '*') {
+                            ans.add(a * b);
+                        } else if (c == '+') {
+                            ans.add(a + b);
+                        } else {
+                            ans.add(a - b);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (ans.size() == 0) {
+            ans.add(Integer.parseInt(expression));
+        }
+
+        return ans;
+    }
+}
