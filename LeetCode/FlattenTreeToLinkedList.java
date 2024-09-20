87cd23e995b3bdb41b8a913fce71b86294a3024c
ishantSikdar <ishant9715@gmail.com>
2024-09-20 23:56:09 +0530

    Flatten Tree

--- LeetCode/FlattenTreeToLinkedList.java
+++ LeetCode/FlattenTreeToLinkedList.java
@@ -1,18 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
 
     TreeNode head = null;
@@ -46,6 +51,5 @@ class Solution {
         traverse(root.left);
         traverse(root.right);
     }
-
 
 }
