6482403d2b1f745cb119a9e20d8bbf6694ca79d7
ishantSikdar <ishant9715@gmail.com>
2024-10-03 21:22:19 +0530

    Rank Transfrom Array

---
+++ LeetCode/RankTransformAnArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        for (int i = 0; i<sorted.length; i++) {
+            map.putIfAbsent(sorted[i], map.size() + 1);
+        }
+
+        for (int i = 0; i<arr.length; i++) {
+            arr[i] = map.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
