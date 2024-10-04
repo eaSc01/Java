008311b082f14f41b5abdc5f0cabe6df94ab0aa2
ishantSikdar <ishant9715@gmail.com>
2024-10-04 14:34:41 +0530

    Divide players of equal skills

---
+++ LeetCode/DividePlayersOfEqualSkill.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+
+        int left = 0;
+        int right = skill.length - 1;
+
+        int sum = skill[left] + skill[right];
+        long chemistry = 0;
+
+        while (left < right) {
+            if (skill[left] + skill[right] != sum) {
+                return -1;
+            }
+
+            chemistry += (long) skill[left++] * skill[right--];
+        }
+
+        return chemistry;
+    }
+}
